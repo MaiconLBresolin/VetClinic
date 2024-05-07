eea07726e277829fb369abf2dfd445e7ad1166ee
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-05-07 17:21:24 -0300

    feat: adding database connection

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const db = require("./db/connection")
+
+const PORT = 3000
+
+app.listen(PORT, function () {
+  console.log(`O express está rodando na porta ${PORT}`)
+})
+
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conecto ao banco com sucesso")
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao se conectar", err)
+  })
+
+//rotas
+app.get("/", (req, res) => {
+  res.send("Está Funcionando!")
+})
