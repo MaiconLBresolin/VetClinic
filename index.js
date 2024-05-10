17a34179b549bc2798327cefd00fd6daa6bba65c
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-05-10 00:38:47 -0300

    refactor: add routes to main page

--- index.js
+++ index.js
@@ -7,19 +7,37 @@ const db = require("./db/connection")
 const PORT = 3000
 
 app.listen(PORT, function () {
-  console.log(`O express está rodando na porta ${PORT}`)
+  console.log(`O app está ouvindo a porta ${PORT}`)
 })
 
-//db connection
 db.authenticate()
   .then(() => {
-    console.log("Conectou ao banco com sucesso")
+    console.log("Conectou-se ao banco de dados com sucesso")
   })
   .catch((err) => {
-    console.log("Ocorreu um erro ao se conectar", err)
+    console.log("Ocorreu um erro ao conectar-se ao banco de dados")
   })
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(express.json())
+app.use(express.static("public"))
 
 //rotas
 app.get("/", (req, res) => {
   res.send("Está Funcionando!")
 })
+
+/*db.sync({ force: false })
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
+*/
+
+app.use("/pet", require("./routes/Pet"))
+app.use("/tutor", require("./routes/Tutor"))
