43dc93715577c1eaedba55bc672fd08c2b02d24c
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-05-10 00:42:41 -0300

    feat: add get pet

---
+++ routes/Pet.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+//const { getTutors } = require("../controllers/TutorController")
+
+const Pet = require("../models/Pet")
+
+router.get("/search", async (req, res) => {
+  let petList = await Pet.findAll({ raw: true })
+  console.log(petList)
+})
+
+router.post("/add/:id", async (req, res) => {
+  const tutorId = req.params.id
+  const { name, species, carry, weight, date_of_birth } = req.body
+
+  try {
+    await Pet.create({
+      name,
+      species,
+      carry,
+      weight,
+      date_of_birth,
+      idTutor: tutorId,
+    })
+    res.redirect("/")
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+module.exports = router
