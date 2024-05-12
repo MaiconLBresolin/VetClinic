a32240c052b67019ff94ab6e303c9442089a47f7
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-05-11 21:53:41 -0300

    refactor: add service files to suit mvc architecture

--- services/TutorService.js
+++ services/TutorService.js
@@ -1,14 +1,58 @@
-const { Tutor } = require("../models/Tutor")
+const Tutor = require("../models/Tutor")
+const Pet = require("../models/Pet")
+const Sequelize = require("../db/connection")
 
-const getAllTutors = async () => {
+const getTutorList = async (req, res) => {
   try {
-    const tutors = await Tutor.findAll()
-    return tutors
+    const tutorList = await Tutor.findAll({
+      include: Pet,
+    })
+    return tutorList
   } catch (err) {
-    console.log(err)
+    console.error(err)
+    throw err
+  }
+}
+
+const postNewTutor = async (tutorData) => {
+  try {
+    await Tutor.create(tutorData)
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+const updateTutor = async (id, tutorData) => {
+  try {
+    const tutor = await Tutor.findOne({ where: { id } })
+    if (!tutor) {
+      throw new Error("Não existe tutor registrado com esse id")
+    }
+    Object.assign(tutor, tutorData)
+    await tutor.save()
+    console.log("dados atualizados")
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+const deleteTutorAndPets = async (id) => {
+  try {
+    await Sequelize.transaction(async (t) => {
+      await Pet.destroy({ where: { idTutor: id }, transaction: t })
+      await Tutor.destroy({ where: { id: id }, transaction: t })
+    })
+  } catch (err) {
+    console.error(err)
+    throw err
   }
 }
 
 module.exports = {
-  getAllTutors,
+  getTutorList,
+  postNewTutor,
+  updateTutor,
+  deleteTutorAndPets,
 }
